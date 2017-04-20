76e35b165aac241c971312cba9743b770fb401dd
fabricebrito <fabrice.brito@terradue.com>
2017-04-20 10:29:26 +0200

    update

--- src/main/app-resources/sen2cor/lib/functions.sh
+++ src/main/app-resources/sen2cor/lib/functions.sh
@@ -379,6 +379,12 @@ function main() {
         "$( basename "${result}" )" \
         ${target_xml}
 
+      # TODO
+      # metadata_iso \
+      #   "xpath expression" \
+      #   "value" \
+      #   ${target_xml_md}
+
       # set product type
       metadata \
         "//A:EarthObservation/D:metaDataProperty/D:EarthObservationMetaData/D:productType" \
@@ -483,3 +489,20 @@ function metadata() {
  
 }
 
+function metadata_iso() {
+ 
+  local xpath="$1"
+  local value="$2"
+  local target_xml="$3"
+
+  # TODO update this
+  # xmlstarlet ed -L \
+  #  -N A="http://www.opengis.net/opt/2.1" \
+  #  -N B="http://www.opengis.net/om/2.0" \
+  #  -N C="http://www.opengis.net/gml/3.2" \
+  #  -N D="http://www.opengis.net/eop/2.1" \
+  #  -u  "${xpath}" \
+  #  -v "${value}" \
+  #  ${target_xml}
+
+}
